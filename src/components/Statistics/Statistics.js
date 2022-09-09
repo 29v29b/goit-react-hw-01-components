dd7d6669914c23304305ee47e7634ab995ac05e3
Victoria Bulakh <bulakh.victoria29@gmail.com>
2022-09-09 09:45:04 +0300

    Add App.js and components

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,28 @@
+import css from './Statistics.module.css'
+import PropTypes from "prop-types";
+
+export const Statistics = ({title, stats}) => {
+    return <section className={css.statistics}>
+    <h2 className={css.title}>{title}</h2>
+  
+    <ul className={css.stats}>
+        {stats.map(({ id, label, percentage }, index) => (
+          <li className={css.item} key={id}>
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      }).isRequired
+    ).isRequired,
+  };
